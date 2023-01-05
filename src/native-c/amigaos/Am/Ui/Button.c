aea4e2c9e8c3b0e2aaac7ab8baca40f38506b8cd
Anders Kjeldsen <anders@kelson.no>
2023-01-05 08:56:39 +0100

    base class testing

--- src/native-c/amigaos/Am/Ui/Button.c
+++ src/native-c/amigaos/Am/Ui/Button.c
@@ -15,13 +15,14 @@ function_result Am_Ui_Button__native_init_0(aobject * const this)
 	if (this != NULL) {
 		__increase_reference_count(this);
 	}
-
+/*
     struct NewGadget new_gad[1] = { 63, 26, 172, 13, (UBYTE *)"Name", &topaz8, 1, PLACETEXT_LEFT, NULL, NULL };
     struct Gadget *gadget = CreateGadgetA(BUTTON_KIND, ne, &Gadgetdata[i], (struct TagItem *)&GadgetTags[i])) {
             printf("Gadget %d created.\n", i);
          }
          else
             printf("Failed to create gadget %d.\n",i);
+			*/
 __exit: ;
 	if (this != NULL) {
 		__decrease_reference_count(this);
