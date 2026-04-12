42bfca503bea25aadf3477436a5758f90fcf1c15
anderskjeldsen <mbp2nfuqoku3zbj2pkbicrgsx24kq7p2voq5mm4dxgli7yfhvtha>
2026-04-12 12:23:26 +0200

    PixelBufferView with example

---
+++ src/native-c/morphos-ppc/Am/Ui/PixelBufferView.c
@@ -0,0 +1,54 @@
+#include <libc/core.h>
+#include <Am/Ui/PixelBufferView.h>
+#include <morphos-ppc/Am/Ui/PixelBufferView.h>
+#include <libc/core_inline_functions.h>
+
+// PixelBufferView is AmigaOS/CyberGraphX-only for now.
+// MorphOS stubs — no-ops.
+
+function_result Am_Ui_PixelBufferView__native_init_0(aobject * const this)
+{
+    function_result __result = { .has_return_value = false };
+    bool __returning = false;
+__exit: ;
+    return __result;
+}
+
+function_result Am_Ui_PixelBufferView__native_release_0(aobject * const this)
+{
+    function_result __result = { .has_return_value = false };
+    bool __returning = false;
+__exit: ;
+    return __result;
+}
+
+function_result Am_Ui_PixelBufferView__native_mark_children_0(aobject * const this)
+{
+    function_result __result = { .has_return_value = false };
+    bool __returning = false;
+__exit: ;
+    return __result;
+}
+
+function_result Am_Ui_PixelBufferView_initBuffer_0(
+    aobject * const this,
+    unsigned short width,
+    unsigned short height)
+{
+    function_result __result = { .has_return_value = false };
+    bool __returning = false;
+__exit: ;
+    return __result;
+}
+
+function_result Am_Ui_PixelBufferView_writePixels_0(
+    aobject * const this,
+    aobject * graphics,
+    short x,
+    short y)
+{
+    function_result __result = { .has_return_value = false };
+    bool __returning = false;
+__exit: ;
+    return __result;
+}
